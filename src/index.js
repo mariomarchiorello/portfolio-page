d5ed8a7b190e40e3fb46fc9faa6fad258de818f6
Mario Marchiorello <mario.marchiorello@gmail.com>
2021-09-15 09:14:21 +0200

    change index.js and remove redux

--- src/index.js
+++ src/index.js
@@ -1,15 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import {Provider} from "react-redux";
-import store from "./Store";
 import {GlobalStyle} from "./Styles/globalstyle";
 
 ReactDOM.render(
-  <Provider store={store}>
-    <GlobalStyle/>
-    <App />
-  </Provider>,
+    <>
+        <GlobalStyle/>
+        <App />
+    </>,
   document.getElementById('root')
 );
 
