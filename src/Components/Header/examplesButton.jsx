9695e0c9ab74edb4df74eddcc93eb6bb677dd24a
Mario Marchiorello <mario.marchiorello@gmail.com>
2021-11-03 13:33:51 +0100

    rebuild header menu

---
+++ src/Components/Header/examplesButton.jsx
@@ -0,0 +1,58 @@
+import React, {useState} from 'react';
+import Button from '@mui/material/Button';
+import Menu from '@mui/material/Menu';
+import MenuItem from '@mui/material/MenuItem';
+import { useHistory } from 'react-router-dom';
+
+
+function ExamplesButton() {
+
+    const [anchorEl, setAnchorEl] = useState(null)
+    const history = useHistory()
+    const open = Boolean(anchorEl)
+    const handleClick = event => {
+        event.preventDefault()
+        setAnchorEl(event.currentTarget)
+    }
+    const goToPage = goTo => {
+        history.push(goTo)
+        setAnchorEl(null)
+    }
+    const handleClose =()=>{
+        setAnchorEl(null)
+    }
+
+    return (
+        <>
+            <Button
+              sx={{color:"black"}}
+              id="menuButton"
+              aria-controls="examples-menu"
+              aria-haspopup="true"
+              aria-expanded={open ? 'true' : undefined}
+              onClick={handleClick}
+            >examples
+            </Button>
+            <Menu
+              id="examples-menu"
+              anchorEl={anchorEl}
+              open={open}
+              onClose={handleClose}
+              MenuListProps={{
+                'aria-labelledby': 'basic-button',
+              }}
+            >
+              <MenuItem onClick={() => goToPage('/books')}>
+                Library Challenge
+              </MenuItem>
+              <MenuItem disabled={true} onClick={() => goToPage('/')}>
+                  Starwars API
+              </MenuItem>
+            </Menu>
+
+
+        </>
+    );
+}
+
+export default ExamplesButton;
--- src/Components/Header/header.jsx
+++ src/Components/Header/header.jsx
@@ -5,6 +5,8 @@ import Button from '@mui/material/Button';
 import Menu from '@mui/material/Menu';
 import MenuItem from '@mui/material/MenuItem';
 import { useHistory } from 'react-router-dom';
+import AboutMeButton from "./aboutMeButton";
+import ExamplesButton from "./examplesButton";
 
 function Header(props) {
   const [anchorEl, setAnchorEl] = useState(null);
@@ -27,41 +29,10 @@ function Header(props) {
         </section>
 
         <section className="menu">
-          <div>
-          <Button
-              sx={{color:"black"}}
-              id="basic-button">About me</Button>
-        </div>
-        <div>
-            <Button
-              sx={{color:"black"}}
-              id="menuButton"
-              aria-controls="basic-menu"
-              aria-haspopup="true"
-              aria-expanded={open ? 'true' : undefined}
-              onClick={handleClick}
-            >
-              Examples
-            </Button>
-            <Menu
-              id="basic-menu"
-              anchorEl={anchorEl}
-              open={open}
-              onClose={handleClose}
-              MenuListProps={{
-                'aria-labelledby': 'basic-button',
-              }}
-            >
-              <MenuItem onClick={() => handleClose('/books')}>
-                Library challenge
-              </MenuItem>
-              <MenuItem
-                  disabled={true}
-                  onClick={() => handleClose('/books')}>
-                starwars API
-              </MenuItem>
-            </Menu>
-          </div>
+
+            <AboutMeButton/>
+
+            <ExamplesButton/>
         </section>
       </Container>
     </>
